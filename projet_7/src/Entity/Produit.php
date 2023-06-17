b37eebe3f62457cc37e20da497ec494543b6819d
Alexandre-Errecade <alexandre.errecade@tut-tut.com>
2023-06-17 11:27:07 +0200

    #1 create database

---
+++ projet_7/src/Entity/Produit.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProduitRepository::class)
+ */
+class Produit
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true, unique = true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $reference;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(?string $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(?string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+}
---
+++ projet_7/src/Traits/Timestampable.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Traits;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use JMS\Serializer\Annotation as Serializer;
+
+/* @Gedmo\Timestampable(on="create") */
+
+trait Timestampable
+{
+  /**
+   * @ORM\Column(type="datetime",nullable=true)
+   *  @Gedmo\Timestampable(on="create")
+   * @Serializer\Expose
+   * @Serializer\MaxDepth(1)
+   *
+   */
+    protected $createdAt;
+
+  /**
+   * @ORM\Column(type="datetime",nullable=true)
+   *  @Gedmo\Timestampable(on="update")
+   * @Serializer\Expose
+   * @Serializer\MaxDepth(1)
+   *
+   */
+    protected $updatedAt;
+
+  /**
+   * @param \DateTime $createdAt
+   * @return mixed
+   */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+  /**
+   * @return mixed
+   */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+  /**
+   * @param \DateTime $updatedAt
+   * @return mixed
+   */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+  /**
+   * @return mixed
+   */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
