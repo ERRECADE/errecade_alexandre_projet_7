0147fad65ca56eccaad36b1478f6c8569f1fd4d3
Alexandre-Errecade <alexandre.errecade@tut-tut.com>
2023-06-23 18:57:02 +0200

    5_Adding_and_Deleting_a_User

--- projet_7/src/Controller/UtilisateurController.php
+++ projet_7/src/Controller/UtilisateurController.php
@@ -1,18 +1,22 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\Utilisateur;
+use DateTime;
 use App\Entity\Client;
+use App\Entity\Utilisateur;
 use App\Repository\ClientRepository;
-use App\Repository\UtilisateurRepository;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Security\Core\Security;
+use App\Repository\UtilisateurRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 /**
@@ -24,13 +28,16 @@ class UtilisateurController extends AbstractController
     private $clientRepository;
     private $serializer;
     private $utilisateurRepository;
+    private $validator;
 
-    public function __construct(Security $security, ClientRepository $clientRepository,SerializerInterface $serializer,UtilisateurRepository $utilisateurRepository)
+
+    public function __construct(Security $security, ClientRepository $clientRepository,SerializerInterface $serializer,UtilisateurRepository $utilisateurRepository, ValidatorInterface $validator)
     {
         $this->security = $security;
         $this->clientRepository = $clientRepository;
         $this->serializer = $serializer;
         $this->utilisateurRepository = $utilisateurRepository;
+        $this->validator = $validator;
     }
 
     /**
@@ -51,7 +58,7 @@ class UtilisateurController extends AbstractController
     }
 
     /**
-     * @Route("/detail", name="utilisateur_detail", methods={"POST"})
+     * @Route("/detail", name="detail", methods={"POST"})
      * Récupération d'un seul utilisateur
      */
     public function getUtilisateur(Request $request)
@@ -70,4 +77,83 @@ class UtilisateurController extends AbstractController
             }
         }
     }
+
+    /**
+     * @Route("/delets", name="delets", methods={"DELETE"})
+     * Suprimez un utilisateur
+     */
+    public function deleteUtilisateur(Request $request)
+    {
+        $client = $this->security->getUser();
+        $data = json_decode($request->getContent(), true);
+        $id = $data['id'];
+
+        if ($id) {
+            $utilisateur = $this->utilisateurRepository->findOneByUtilisateur($id, $client);
+            if ($utilisateur) {
+                $this->utilisateurRepository->remove($utilisateur,true);
+                return new JsonResponse("Utilisateur suprimé.", Response::HTTP_OK);
+            } else {
+                return new JsonResponse("Utilisateur non trouvé.", Response::HTTP_NOT_FOUND);
+            }
+        }
+    }
+
+    /**
+     * @Route("/create", name="create", methods={"POST"})
+     * Crée un utilisateur
+     */
+    public function createUtilisateur(Request $request): JsonResponse 
+    {
+
+        $utilisateur = $this->serializer->deserialize($request->getContent(), Utilisateur::class, 'json');
+        $content = $request->toArray();
+        $emailClient = $this->security->getUser()->getUserIdentifier();
+
+        $errors = $this->validator->validate($utilisateur);
+
+        if ($errors->count() > 0) {
+            return new JsonResponse($this->serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+
+        $utilisateur->setClient($this->clientRepository->findOneBy(["email" => $emailClient]));
+        $utilisateur->setActif(1);
+        $utilisateur->setIsDeleted(0);
+        $utilisateur->setCreatedAt(new DateTime);
+
+        $this->utilisateurRepository->add($utilisateur,true);
+
+        return new JsonResponse("l'utilisateur a bien était ajouter", Response::HTTP_CREATED);
+   }
+
+    /**
+     * @Route("/update", name="update", methods={"PUT"})
+     * Modifier un utilisateur
+    */
+
+   public function updateUtilisateur(Request $request): JsonResponse
+   { 
+    $id = $request->query->get('id');
+    $client = $this->security->getUser();
+    $utilisateur = $this->utilisateurRepository->findOneByUtilisateur($id, $client);
+
+    if (!$utilisateur) {
+        return new JsonResponse("Utilisateur non trouvé.", Response::HTTP_NOT_FOUND);
+    }
+
+    $updatedUtilisateur = $this->serializer->deserialize(
+        $request->getContent(),
+        Utilisateur::class,
+        'json',
+        [AbstractNormalizer::OBJECT_TO_POPULATE => $utilisateur]
+    );
+
+    $updatedUtilisateur->setClient($client);
+    $updatedUtilisateur->setUpdatedAt(new DateTime);
+
+    $this->utilisateurRepository->add($updatedUtilisateur, true);
+
+    return new JsonResponse("L'utilisateur a bien été mis à jour.", Response::HTTP_OK);
+
+  }
 }
